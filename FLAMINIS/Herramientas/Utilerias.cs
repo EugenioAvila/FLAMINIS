c11f70e830ae8cb338e1b0b990f61c069510c54d
Eugenio Avila <avilamqz@hotmail.com>
2018-05-09 09:59:48 -0700

    Definida clase de utilerias

---
+++ FLAMINIS/Herramientas/Utilerias.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace FLAMINIS.Herramientas
+{
+    public class Utilerias
+    {
+        public class cComboBox
+        {
+            public string DESCR { get; set; }
+            public short ID { get; set; }
+        }
+    }
+}
